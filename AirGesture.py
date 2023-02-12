615fced7e73661517315d35b6934854351445a6a
Bui Trung Hieu <120043377+btrunghieu@users.noreply.github.com>
2023-02-12 22:53:49 +0700

    Update AirGesture.py

--- AirGesture.py
+++ AirGesture.py
@@ -7,20 +7,14 @@ mp_hands = mp.solutions.hands
 
 kb = Controller()
 
-# For static images:
 IMAGE_FILES = []
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=2,
     min_detection_confidence=0.5) as hands:
   for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
     image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Print handedness and draw hand landmarks on the image.
     print('Handedness:', results.multi_handedness)
     if not results.multi_hand_landmarks:
       continue
@@ -41,14 +35,12 @@ with mp_hands.Hands(
           mp_drawing_styles.get_default_hand_connections_style())
     cv2.imwrite(
         '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-    # Draw hand world landmarks.
     if not results.multi_hand_world_landmarks:
       continue
     for hand_world_landmarks in results.multi_hand_world_landmarks:
       mp_drawing.plot_landmarks(
         hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
 
-# For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     model_complexity=0,
@@ -58,16 +50,12 @@ with mp_hands.Hands(
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+        
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
-    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
@@ -86,7 +74,7 @@ with mp_hands.Hands(
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
+
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
